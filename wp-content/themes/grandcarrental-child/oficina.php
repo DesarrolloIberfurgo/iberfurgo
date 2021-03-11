1fb7a71e5db34e50cec23acf56be161be05a6cdf
Rafael Baldayo Arago <rbaldayo@gmail.com>
2021-03-11 17:29:18 +0100

    pagina oficina

--- wp-content/themes/grandcarrental-child/oficina.php
+++ wp-content/themes/grandcarrental-child/oficina.php
@@ -15,8 +15,28 @@ if ($httpCode != 200) {
 	return 'ha petado';
 }
 
-echo $response->data[0]->direccion;
+
 
 ?>
 
+<div class="inner">
+    <div class="inner_wrapper nopadding">
+        <?php
+            if(!empty($response->data))
+            {
+        ?>
+                <div class="standard_wrapper"></div><br class="clear"/><br/>
+        <?php
+            }
+        ?>
+                <div id="page_main_content" class="sidebar_content full_width fixed_column">
+
+                    <div class="standard_wrapper">
+                    <?php 
+                        echo $response->data[0]->direccion;
+                    ?>
+                    </div> <!-- standard_wrapper -->
+                </div>
+    </div> <!-- inner_wrapper -->
+</div> <!-- inner -->
 <?php get_footer(); ?>
