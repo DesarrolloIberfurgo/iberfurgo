2e541611963a0b6e3a31ecf99ccc8b36adb8329a
Rafael Baldayo Arago <rbaldayo@gmail.com>
2021-03-17 18:23:53 +0100

    oficina disenyo

--- wp-content/themes/grandcarrental-child/oficina.php
+++ wp-content/themes/grandcarrental-child/oficina.php
@@ -24,6 +24,7 @@ if ($httpCode != 200) {
         <?php
             if(!empty($response->data))
             {
+                $value = $response->data[0];
         ?>
                 <div class="standard_wrapper"></div><br class="clear"/><br/>
         <?php
@@ -32,9 +33,22 @@ if ($httpCode != 200) {
                 <div id="page_main_content" class="sidebar_content full_width fixed_column">
 
                     <div class="standard_wrapper">
-                    <?php 
-                        echo $response->data[0]->direccion;
-                    ?>
+                        <div class="one">
+                            <div class="one_third">
+                            <?php 
+                                echo $value->direccion;
+                            ?>
+                            </div>
+                            <div class="one_third">
+                            <h3>Horario</h3>
+                            pepe
+                            </div>
+                            <div class="one_third last">
+                            <?php 
+                                echo $value->direccion;
+                            ?>
+                            </div>
+                        </div>
                     </div> <!-- standard_wrapper -->
                 </div>
     </div> <!-- inner_wrapper -->
